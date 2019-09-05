3b29acfd6a68b7241cb0f7eb270786860ef36e12
Filip Babic <fbabic96@gmail.com>
2019-09-05 14:52:32 +0200

    Added CORS workaround

--- app.js
+++ app.js
@@ -28,6 +28,14 @@ const noteController = note(NoteModel)
 //application & routing
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 require('./routes/index')(app, {
     userController: userController,
--- db/index.js
+++ db/index.js
@@ -3,5 +3,7 @@ const config = require('../config')
 const database = {}
 
 database.connectToDatabase = database => database.connect(config.DB_URL)
+    .then(() => console.log("connected to database"))
+    .catch((error) => console.log(error))
 
 module.exports = database
