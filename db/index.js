ebd8f151b2753181a33a31e00237edeb0d330c86
Filip Babic <fbabic96@gmail.com>
2020-11-08 14:18:29 +0100

    Updated MongoDB

--- db/index.js
+++ db/index.js
@@ -1,9 +1,15 @@
-const config = require('../config')
-
 const database = {}
 
-database.connectToDatabase = database => database.connect(config.DB_URL)
-    .then(() => console.log("connected to database"))
+const uri = "mongodb+srv://heroku_4xs44bgj:Mmo66920f@cluster-4xs44bgj.if8cx.mongodb.net/heroku_4xs44bgj?retryWrites=true&w=majority"
+
+database.connectToDatabase = database => database.connect(
+    uri, {
+        useNewUrlParser: true
+    })
+    .then((database) => {
+        console.log("connected to database")
+        console.log(database)
+    })
     .catch((error) => console.log(error))
 
 module.exports = database
