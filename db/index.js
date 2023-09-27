63ce2d97ff8bcebb0d14e033d7f3012a52c82d62
Zac Lippard <zlippard@gmail.com>
2023-09-27 12:14:19 -0700

    Update index.js

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const database = {}
 
-const uri = "mongodb+srv://heroku_4xs44bgj:Mmo66920f@cluster-4xs44bgj.if8cx.mongodb.net/heroku_4xs44bgj?retryWrites=true&w=majority"
+// const uri = "mongodb+srv://heroku_4xs44bgj:Mmo66920f@cluster-4xs44bgj.if8cx.mongodb.net/heroku_4xs44bgj?retryWrites=true&w=majority"
+const uri = "mongodb+srv://zlippard:IX6ugwTZYPYSeQjZ@taskie-kodeco-db.ga6e3bg.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"
 
 database.connectToDatabase = database => database.connect(
     uri, {
